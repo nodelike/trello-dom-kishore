52b8f36fd62717a12ace898544fbcb7fb7fa4299
SenpaiKishore <108696612+SenpaiKishore@users.noreply.github.com>
2024-06-11 12:40:27 +0530

    Adding custom classes for Tailwind CSS.

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+tailwind.config = {
+    darkMode: 'selector',
+    theme: {
+        extend: {
+            fontFamily: {
+                poppins: ["Poppins", "sans-serif"],
+            },
+            boxShadow: {
+                center: '0px 0px 25px rgba(0, 0, 0, 0.2)',
+                darkcenter: '0px 0px 30px rgba(255, 103, 0, 0.7)'
+            },
+            colors: {
+                lime: 'rgb(255, 103, 0)',
+                dark: 'rgb(12, 22, 25)',
+                glare: 'rgba(0, 0, 0, 0.2)'
+            }
+        },
+    },
+};
